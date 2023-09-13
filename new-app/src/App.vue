d9f71f9d450a3261614cff8b986ea8474b4aa244
shayramos <shayramos@hotmail.com>
2023-09-13 17:31:25 +0100

    initial

---
+++ new-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <router-view></router-view>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+</template>
+
+<!-- <script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script> -->
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
